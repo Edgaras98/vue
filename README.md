94dc88da3c3a542d246c8ea9024b115f8d47d836
Edgaras Stepanovas <78696812+Edgaras98@users.noreply.github.com>
2022-02-03 14:27:06 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,24 +1,13 @@
-# vue
+# Project
+
+Simple weather app using https://openweathermap.org/api api.
+Type city name or country you want see weather conditions (api is set for metric values)
+
+## Framework
+
+Vue 3.0
 
 ## Project setup
-```
-npm install
-```
 
-### Compiles and hot-reloads for development
-```
 npm run serve
-```
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,157 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :class="setBg()">
+  <main>
+
+    <div class="search-box">
+      <input 
+          type="text" 
+          class="search-bar" 
+          placeholder="Search..."
+          v-model="query"
+          @keyup.enter="fetchWeather"
+        />
+    </div> 
+
+    <div class="weather-wrap" v-if="this.weather.main != undefined">
+        <div class="location-box">
+          <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
+          <div class="date">{{this.date}}</div>
+        </div>
+         <div class="weather-box">
+      <div class="temp">{{Math.round(weather.main.temp)}}°C</div>
+      <div class="weather">{{weather.weather[0].main}}</div>
+    </div>
+    </div>
+
+  </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+  return {
+    api_key: process.env.VUE_APP_API_KEY,
+    url_base: 'https://api.openweathermap.org/data/2.5/',
+    query: '',
+    weather: {},
+    date: new Date().toJSON().slice(0,10)
+   }
+   },
+  methods:{
+    async fetchWeather() {
+      const res = await fetch(`${this.url_base}weather?q=${this.query}&units=metric&appid=${this.api_key}`)
+      this.weather = await res.json();
+    },
+
+    setBg(){
+   return this.weather.main != undefined && this.weather.main.temp >= 10 ? 'warm': '' 
+    },
+
   }
-}
+} 
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;1,200&display=swap');
+
+*{
+  font-family: 'Montserrat', sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+
+#app{
+  background-image: url('./assets/images/cold-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+#app .warm{
+  background-image: url('./assets/images/warm-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main{
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75));
+}
+
+.search-box{
+  width: 75vw;
+  margin: 0 auto;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar{
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: black;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  box-shadow: 0px 0px 8px rgba(255, 255, 255, 0.25);
+  background: none;
+  background-color: rgba(255, 255, 255, 0.726);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box  .search-bar:focus{
+  background-color: rgba(255, 255, 255, 0.75);
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location{
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25)
+}
+
+.location-box .date{
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  text-align: center;
+  font-style: italic;
+}
+
+.weather-box{
+  text-align: center;
+}
+
+.weather-box .temp{
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0,0,0,0.25);
+  background-color: rgba(255,255,255,0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather{
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
 </style>
